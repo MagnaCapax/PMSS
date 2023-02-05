4dc646137ddf5ae80211d31b612d2016252d8c5d
MagnaCapax <123067457+MagnaCapax@users.noreply.github.com>
2023-02-05 14:44:39 +0200

    removed legacy, rewording

--- soft.sh
+++ soft.sh
@@ -1,4 +1,4 @@
-echo "### Verifying validity -> emptying directories etc."
+echo "### Installing scripts, configs and skel\n"
 rm -rf /etc/skel/*
 #rm -rf /scripts/*
 #rm -rf /var/www/proxy
@@ -12,11 +12,3 @@ cp -r var /
 chmod o-rwx /scripts
 chmod o-rwx /scripts/*
 chmod o-rwx /root/*
-
-#echo "### Installing proxy"
-#cd /var/www
-#mkdir proxy
-#cd proxy
-#wget http://www.sproowl.net/files/sproowl-dimension.tar.gz
-#tar -zxf sproowl-dimension.tar.gz
-#rm -rf sproowl-dimension.tar.gz
