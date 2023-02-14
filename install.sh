3d4d9f47c4e2fe027b9d7a521b5002dec5a5a6da
gydis <egor.eremin@aalto.fi>
2023-02-14 15:08:08 +0200

    Add choice of release vs git branch to install.sh

--- install.sh
+++ install.sh
@@ -79,8 +79,16 @@ echo "### Setting up software"
 mkdir ~/compile
 cd /tmp
 rm -rf PMSS*
-git clone https://github.com/MagnaCapax/PMSS
-cd PMSS
+if [ "${1:0:3}" = "git" ]; then
+    git clone https://github.com/MagnaCapax/PMSS;
+    cd PMSS;
+    git checkout "${1:4}";
+else
+    wget https://api.github.com/repos/MagnaCapax/PMSS/releases/latest -O - | awk -F \" -v RS="," '/tarball_url/ {print $(NF-1)}' | xargs wget -O PMSS.tar.gz;
+    mkdir PMSS && tar -xzf PMSS.tar.gz -C PMSS --strip-components 1;
+    cd PMSS;
+fi
+
 bash soft.sh
 
 #TODO Transfer over the proper rc.local + sysctl configs
