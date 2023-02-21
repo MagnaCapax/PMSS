6dbeca8fd6571d6a8f44c7a116b50cde9b0c4c41
gydis <egor.eremin@aalto.fi>
2023-02-21 14:28:36 +0200

    Fix update.php to support source switching and correct version logging

--- scripts/update.php
+++ scripts/update.php
@@ -33,39 +33,45 @@ $sourceVersion = file_get_contents('/etc/seedbox/config/version');
 if (! empty($argv[1]))
     $sourceVersion = $argv[1];
 
+$source = explode(' ', $sourceVersion);
+$source = $source[0];
+
 switch (true) {
-    case stristr($sourceVersion, 'release'):
-	passthru(<<<EOF
-	cd /tmp;
-	rm -rf PMSS*;
-	VERSION=$(wget https://api.github.com/repos/MagnaCapax/PMSS/releases/latest -O - | awk -F \" -v RS="," '/tag_name/ {print $(NF-1)}')
-	wget "https://api.github.com/repos/MagnaCapax/PMSS/tarball/${VERSION}" -O PMSS.tar.gz;
-	mkdir PMSS && tar -xzf PMSS.tar.gz -C PMSS --strip-components 1;
-	SOURCE="release"
-	echo "$SOURCE $VERSION" > /etc/seedbox/config/version;
-	EOF
-	);
-    case stristr($sourceVersion, 'git'):
-	$gitBranch = _(explode(' ', $sourceVersion)[0]); // Get git/branch string
-	$branch = _(substr($gitBranch, 4)); // Get branch string
-	passthru(<<<EOF
-	cd /tmp;
-	rm -rf PMSS*;
-	git clone https://github.com/MagnaCapax/PMSS;
-	cd PMSS;
-	git checkout $branch;
-	SOURCE=$gitBranch;
-	VERSION=$(date);
-	echo "$SOURCE $VERSION" > /etc/seedbox/config/version;
-	EOF
-	);
+    case (substr($source,0,7) == "release"):
+	echo "Using releases as the source!\n";
+	$newVersion = trim(shell_exec(<<<EOF
+	    wget https://api.github.com/repos/MagnaCapax/PMSS/releases/latest -O - | awk -F \" -v RS="," '/tag_name/ {print $(NF-1)}'
+	EOF));
+	$source = "release";
+	$script = <<<EOF
+	    cd /tmp;
+	    rm -rf PMSS*;
+	    wget "https://api.github.com/repos/MagnaCapax/PMSS/tarball/{$newVersion}" -O PMSS.tar.gz;
+	    mkdir PMSS && tar -xzf PMSS.tar.gz -C PMSS --strip-components 1;
+	    echo "{$source} {$newVersion}" > /etc/seedbox/config/version;
+	EOF;
+	passthru($script);
+	break;
+
+    case (substr($source, 0, 3) == "git"):
+	echo "Using GitHub branch as the source!\n";
+	$branch = substr($source, 4); // Get branch string
+	$date = date("D M j G:i:s T Y");
+	$script = <<<EOF
+	    cd /tmp;
+	    rm -rf PMSS*;
+	    git clone https://github.com/MagnaCapax/PMSS;
+	    cd PMSS;
+	    git checkout {$branch};
+	    echo "{$source} {$date}" > /etc/seedbox/config/version;
+	EOF;
+	passthru($script);
 	break;
 }
 
 # Following is now dynamic because it was just fetched and updated
 # TODO soft.sh kinda outdated now ... Should remove it
 passthru('bash /tmp/PMSS/soft.sh');
-
 
 if (file_exists('/scripts/util/update-step2.php'))
     require '/scripts/util/update-step2.php';
