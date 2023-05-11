de24a3e6bd6e3df225d091f1624cb11770411fea
MagnaCapax <aleksi@magnacapax.fi>
2023-05-11 14:36:00 +0300

    update fixes

--- scripts/util/update-step2.php
+++ scripts/util/update-step2.php
@@ -8,7 +8,6 @@ $users = explode("\n", trim($users));
 $debianVersion = file_get_contents('/etc/debian_version');
 $serverHostname = trim( file_get_contents('/etc/hostname') );
 
-
 //Hacky thing due to a bug in github version not getting updated when refactored.
 #TODO Remove around 05/2024
 $updateSource = file_get_contents('/scripts/update.php');
@@ -17,6 +16,10 @@ if (strpos($updateSource, 'soft.sh') !== 0) {    // Still running old version! F
     passthru('/scripts/update.php');
     die();   // Avoid infinite loop :)
 }
+
+
+
+
 
 // If var run does not exist, create it. Deb8 likes to remove this if empty?
 if (!file_exists('/var/run/pmss'))
