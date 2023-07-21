4c9f7d9c32f3596beb30d5b40712ad11601d9036
MagnaCapax <123067457+MagnaCapax@users.noreply.github.com>
2023-07-21 12:11:22 +0300

    Update checkLighttpdInstances.php
    Check for sockets etc. turns out many times sockets don't start etc.  so until some php.ini changes etc. done this check is not ready for production.
    remove commentation when ready for production

--- scripts/cron/checkLighttpdInstances.php
+++ scripts/cron/checkLighttpdInstances.php
@@ -1,5 +1,10 @@
 #!/usr/bin/php
 <?php
+/*
+Pulsed Media Seedbox Management Software "PMSS"
+This script manages and monitors user-specific lighttpd and php-cgi processes.
+*/
+
 echo date('Y-m-d H:i:s') . ': Checking Lighttpd instances' . "\n";
 
 // Get & parse users list
@@ -9,6 +14,7 @@ $changedConfig = array();
 
 foreach($users AS $thisUser) {    // Loop users checking their instances
     if (empty($thisUser)) continue;
+    #TODO Uh Oh next one should be separate script :) This is separate task altogether. Works here too as expected, just a bit confusing
     if (file_exists("/home/{$thisUser}/www-disabled") or 
         !file_exists("/home/{$thisUser}/www")) {
             echo "User: {$thisUser} is suspended\n";
@@ -16,33 +22,59 @@ foreach($users AS $thisUser) {    // Loop users checking their instances
             continue;  //Suspended
     }
     
-    $instances = shell_exec('pgrep -u' . $thisUser . ' lighttpd');
-    //echo "Instances:\n{$instances}\n";
-    $instancesPhp = shell_exec("pgrep -u{$thisUser} php-cgi");
-    if (empty($instancesPhp) &&
-        !empty($instances) ) shell_exec("killall -9 -u{$thisUser} lighttpd");
+    $instancesLighttpd = shell_exec("pgrep -u {$thisUser} lighttpd");
+    $instancesPhpCgi = shell_exec("pgrep -u {$thisUser} php-cgi");
+    $socketError = false;
 
-// Let's actually test we get 401 auth requested!
+    // If socket connection fails or no php-cgi instance is found
+    if (empty($instancesPhpCgi)) {        
+            echo "php-cgi not running, for user: {$thisUser}. Killing lighttpd instances.\n";
+            restartLighttpd($thisUser);
+            continue;
 
+    }
+                              
+    // Connect to php-cgi socket
+    /*for ($i = 0; $i < 4; $i++) {  // Adjust the number 4 according to your `max-procs` value
+        $socket = fsockopen("unix:///home/{$thisUser}/.lighttpd/php.socket-$i", 0, $errno, $errstr, 1);
+        if (!$socket or $errno or $errstr) {        
+            echo "Error when attempting to connect to socket /home/{$thisUser}/.lighttpd/php.socket-$i: {$errno}, {$errstr}\n";
+            echo "php-cgi, for user: {$thisUser}. Killing lighttpd instances.\n";            
+            $socketError = true;
+            break;
+        }
+
+        if ($socketError); continue;
+    }
+    if ($socketError); { restartLighttpd($thisUser); continue; }*/
+
+
+
+    // Let's actually test we get 401 auth requested!
+    /* temp disabled, so much log spam and did not achieve desired results.
     $curl = curl_init("http://127.0.0.1/user-{$thisUser}/");
     curl_setopt( $curl, CURLOPT_HEADER, true);
     curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true);
     $httpResponse = curl_exec( $curl );
 
-   if (strpos($httpResponse, 'HTTP/1.1 401 Unauthorized') === false) $instances = '';
-
-
-    if (empty($instancesPhp)) $instances = '';
+   if (strpos($httpResponse, 'HTTP/1.1 401 Unauthorized') === false) $instancesLighttpd = '';
+    */
     
-    if(empty($instances)) {    // No instances at all? Ok time to start rTorrent!
-        start($thisUser);
+    if(empty($instancesLighttpd)) {    // No instances at all? Ok time to start Lighttpd!
+        startLighttpd($thisUser);
         continue;
     }
     
 }
 
 
-function start($user) {    // this actually calls the function to start rTorrent :)
-    echo "Start lighttpd for user: {$user}\n";
-    passthru('/scripts/startLighttpd ' . $user);
+function startLighttpd($thisUser) {    // this actually calls the function to start Lighttpd :)
+    echo "Start lighttpd for user: {$thisUser}\n";
+    passthru('/scripts/startLighttpd ' . $thisUser);
+}
+
+function restartLighttpd($thisUser) {    // Kill any php-cgi or lighttpd process for user, make sure no double launch
+    echo "Killing (if any) lighttpd for user: {$thisUser}\n";
+    shell_exec("killall -15 -u {$thisUser} lighttpd; killall -15 -u {$thisUser} php-cgi; sleep 5; killall -9 -u {$thisUser} lighttpd; killall -9 -u {$thisUser} php-cgi; sleep 0.05;");
+    startLighttpd($thisUser);
 }
