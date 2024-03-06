1cf04f515fcf7b68651703cf0e9009c4eae367eb
MagnaCapax <aleksi@magnacapax.fi>
2024-03-06 13:24:42 +0200

    update some permissions. needs refactoring tho

--- scripts/util/setupSkelPermissions.php
+++ scripts/util/setupSkelPermissions.php
@@ -1,7 +1,12 @@
 #!/usr/bin/php
 <?php
+#TODO Wrong naming etc.
+
 passthru("cd /etc/skel; chmod o-w * -R; chmod o-w .* -R"); // not using 775 because there might be places where the perms differ and need to differ
 passthru("chmod 770 /etc/skel");
+
+passthru("cd /etc/seedbox; chmod o-w * -R; chmod o-w .* -R"); // not using 775 because there might be places where the perms differ and need to differ
+passthru("chmod o+x /etc/seedbox");
 
 // Setup openvpn config perms
 
