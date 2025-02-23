9ab84cfd836eb14ea74b3ffe2b027bded332852f
Glenn Strauss <gstrauss@gluelogic.com>
2025-02-23 05:45:09 -0500

    lighttpd.conf refresh
    x-ref:
      https://github.com/MagnaCapax/PMSS/issues/81
    
    github: closes #81

--- etc/skel/www/lighttpdRestart.php
+++ etc/skel/www/lighttpdRestart.php
@@ -4,5 +4,4 @@ if (!isset($_REQUEST['action'])) die();
 $action = $_REQUEST['action'];
 if ($action !== 'confirm-restart') die();	// double check
 
-//shell_exec('killall -9 -u $(whoami) lighttpd; killall -9 -u $(whoami) php-cgi');
-shell_exec('killall -9 -u $(whoami) lighttpd');
+shell_exec('killall -USR1 -u $(whoami) lighttpd');
