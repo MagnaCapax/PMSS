525acbc0b8e1d67ffcf3ff61b914e01c22777872
Aleksi U <aleksi@magnacapax.fi>
2025-11-11 06:16:40 +0200

    Revert "revert: restore frozen etc/skel/www files to prior snapshot (freeze guard)"
    This reverts commit 617d4a4c6645dca117b835c1ebc817f7c843eda5.

--- etc/skel/www/info.php
+++ etc/skel/www/info.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Seedbox info</title>
-    <link href="https://static.pulsedmedia.com/wc/css/screen.css" rel="stylesheet" type="text/css" media="screen" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>	
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.css" rel="stylesheet" type="text/css" media="screen" />
-    </head>
+    <link href="https://static.pulsedmedia.com/wc/css/screen.css" rel="stylesheet" media="screen" />
+    <!-- Chart.js v4 (UMD build exposes global Chart) -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
+  </head>
 <body>
 	<div id="wrap">
           <div id="full_page">
@@ -32,10 +32,8 @@
     </div><!--Wrap ends -->
 
 <script>
-var refreshId = setInterval(function()
-{
-     $('#stats').fadeOut("slow").load('stats.php').fadeIn("slow");
-}, 600000);
+  // Refresh the page every 10 minutes to update stats without legacy jQuery.
+  window.setInterval(function () { window.location.reload(); }, 600000);
 </script>
 </body>
 </html>
