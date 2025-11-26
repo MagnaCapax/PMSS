829c8ec7b162af680275330b718e46a837bc9d43
Aleksi Ursin <aleksi@magnacapax.fi>
2025-11-26 13:42:49 +0200

    refactor(docker): remove redundant wrapper functions
    - Removed dockerRootlessLog and dockerRootlessStep.
    - Replaced calls with direct logmsg() and runStep() using a '[docker]' prefix.
    - Updated dockerRootlessShellExec to use logmsg directly.

--- scripts/lib/update/apps/docker.php
+++ scripts/lib/update/apps/docker.php
@@ -1,40 +1,26 @@
 <?php
-if (!function_exists('dockerRootlessLog')) {
-    function dockerRootlessLog(string $message): void
-    {
-        if (function_exists('logmsg')) {
-            logmsg('[docker] '.$message);
-        } else {
-            echo '[docker] '.$message."\n";
-        }
-    }
-}
-
-if (!function_exists('dockerRootlessStep')) {
-    function dockerRootlessStep(string $description, string $command): int
-    {
-        return runStep('[docker] '.$description, $command);
-    }
-}
-
 if (!function_exists('dockerRootlessShellExec')) {
     function dockerRootlessShellExec(string $command): string
     {
         $output = shell_exec($command);
-        dockerRootlessLog("Command '{$command}' output: " . trim($output));
+        if (function_exists('logmsg')) {
+            logmsg('[docker] Command \'' . $command . '\' output: ' . trim($output));
+        }
         return $output;
     }
 }
 
-dockerRootlessLog('Starting Docker rootless configuration');
+if (function_exists('logmsg')) {
+    logmsg('[docker] Starting Docker rootless configuration');
+}
 
 // Disable Docker system service and remove stray socket
-dockerRootlessStep('Docker: disabling system service', 'systemctl disable --now docker.service docker.socket');
-dockerRootlessStep('Docker: removing socket file', 'rm -f /var/run/docker.sock');
+runStep('[docker] Docker: disabling system service', 'systemctl disable --now docker.service docker.socket');
+runStep('[docker] Docker: removing socket file', 'rm -f /var/run/docker.sock');
 
 // Enable unprivileged user namespace cloning (rootless requirement)
-dockerRootlessStep('Docker: enabling unprivileged user namespaces', "sh -c 'echo kernel.unprivileged_userns_clone = 1 > /etc/sysctl.d/50-rootless.conf'");
-dockerRootlessStep('Docker: applying sysctl configuration', 'sysctl --system');
+runStep('[docker] Docker: enabling unprivileged user namespaces', "sh -c 'echo kernel.unprivileged_userns_clone = 1 > /etc/sysctl.d/50-rootless.conf'");
+runStep('[docker] Docker: applying sysctl configuration', 'sysctl --system');
 
 // Debian 10 and 11 require additional rootless helpers
 $version = (int) (getenv('PMSS_DISTRO_VERSION') ?: 0);
@@ -45,10 +31,12 @@ if ($version > 0 && $version < 12) {
     // #TODO: Generalize this to a version-managed dependency system
     $url  = 'https://github.com/rootless-containers/slirp4netns/releases/download/v1.3.2/slirp4netns-'.$arch;
     
-    dockerRootlessStep('Docker: downloading slirp4netns helper ('.$arch.')', 'curl -fsSL -o slirp4netns '.escapeshellarg($url));
-    dockerRootlessStep('Docker: installing slirp4netns helper', 'install slirp4netns /usr/local/bin/');
+    runStep('[docker] Docker: downloading slirp4netns helper ('.$arch.')', 'curl -fsSL -o slirp4netns '.escapeshellarg($url));
+    runStep('[docker] Docker: installing slirp4netns helper', 'install slirp4netns /usr/local/bin/');
     @unlink('slirp4netns');
-    dockerRootlessStep('Docker: creating iptables symlink', 'ln -sf /usr/sbin/iptables /usr/local/bin/iptables');
+    runStep('[docker] Docker: creating iptables symlink', 'ln -sf /usr/sbin/iptables /usr/local/bin/iptables');
 }
 
-dockerRootlessLog('Docker rootless configuration complete');
+if (function_exists('logmsg')) {
+    logmsg('[docker] Docker rootless configuration complete');
+}
