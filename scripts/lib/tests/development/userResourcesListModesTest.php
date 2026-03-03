8cf2dbb10e4b316b5a90f2991d5dca7f8a98c1a0
Aleksi Ursin <aleksi@magnacapax.fi>
2026-03-04 00:02:38 +0100

    fix: anchor userResourcesListModesTest source path (Refs #284)

--- scripts/lib/tests/development/userResourcesListModesTest.php
+++ scripts/lib/tests/development/userResourcesListModesTest.php
@@ -5,9 +5,14 @@ require_once __DIR__.'/../common/TestCase.php';
 
 class UserResourcesListModesTest extends TestCase
 {
+    private function loadSource(): string
+    {
+        return (string) file_get_contents(__DIR__.'/../../../util/userResourcesList.php');
+    }
+
     public function testBriefAndFullModesAreWired(): void
     {
-        $src = (string) file_get_contents('scripts/util/userResourcesList.php');
+        $src = $this->loadSource();
         $this->assertStringContainsString("pmssCliOption(", $src);
         $this->assertStringContainsString("'brief'", $src);
         $this->assertStringContainsString("'full'", $src);
@@ -16,19 +21,19 @@ class UserResourcesListModesTest extends TestCase
 
     public function testBriefColumnContractIsPresent(): void
     {
-        $src = (string) file_get_contents('scripts/util/userResourcesList.php');
+        $src = $this->loadSource();
         $this->assertStringContainsString('"User", "UID", "MemHigh", "MemMax", "CPUWt", "CPUQt", "BlkWt", "RdBW", "WrBW", "RdIOPS", "WrIOPS"', $src);
     }
 
     public function testFullColumnContractIsPresent(): void
     {
-        $src = (string) file_get_contents('scripts/util/userResourcesList.php');
+        $src = $this->loadSource();
         $this->assertStringContainsString('"DskQ", "DskB", "InoQ", "InoB", "NetLim", "NetUsed", "ProcMax", "Suspended"', $src);
     }
 
     public function testExtendedJsonFieldsArePresent(): void
     {
-        $src = (string) file_get_contents('scripts/util/userResourcesList.php');
+        $src = $this->loadSource();
         $this->assertStringContainsString("'disk_quota_gib'", $src);
         $this->assertStringContainsString("'network_used_gib'", $src);
         $this->assertStringContainsString("'process_max'", $src);
