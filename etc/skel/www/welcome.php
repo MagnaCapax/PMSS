3ca8e821d166dae368950dd283edf6b7b9aead75
Aleksi Ursin <aleksi@magnacapax.fi>
2026-03-06 10:04:57 +0100

    fix: harden welcome RSS parsing and HTTPS source (Refs #293)

--- etc/skel/www/welcome.php
+++ etc/skel/www/welcome.php
@@ -354,7 +354,7 @@ EOF;
                         <h6>Announcements</h6>
                         <ul>
 <?php
-$rssUrl = 'http://pulsedmedia.com/clients/announcementsrss.php';
+$rssUrl = 'https://pulsedmedia.com/clients/announcementsrss.php';
 $rssContext = stream_context_create(array(
     'http' => array(
         'timeout'    => 5,
@@ -363,6 +363,18 @@ $rssContext = stream_context_create(array(
 ));
 $rssRaw = @file_get_contents($rssUrl, false, $rssContext);
 if ($rssRaw !== false) {
+    if (function_exists('mb_convert_encoding')) {
+        $rssUtf8 = @mb_convert_encoding($rssRaw, 'UTF-8', 'UTF-8');
+        if ($rssUtf8 !== false) {
+            $rssRaw = $rssUtf8;
+        }
+    } elseif (function_exists('iconv')) {
+        $rssUtf8 = @iconv('UTF-8', 'UTF-8//IGNORE', $rssRaw);
+        if ($rssUtf8 !== false) {
+            $rssRaw = $rssUtf8;
+        }
+    }
+
     $rssXml = @simplexml_load_string($rssRaw, 'SimpleXMLElement', LIBXML_NOCDATA);
     if ($rssXml !== false) {
         $rssFeed = json_decode(json_encode($rssXml), true);
